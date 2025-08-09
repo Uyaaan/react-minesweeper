90e9e8fca60c6b6c5805b37598b05fb602624df1
Juliane Lagrimas <uyan@Julianes-MacBook-Air.local>
2025-08-09 23:54:54 +0800

    Build to docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/react-minesweeper/',   // repo name
+  build: { outDir: 'docs' }      // publish build to /docs
 })
